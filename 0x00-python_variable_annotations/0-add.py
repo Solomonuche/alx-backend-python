9bce1e151f7b07fb1385e88592faf27436bf926e
Solomon <solomonuche@gmail.com>
2024-04-19 04:07:42 +0100

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python - Variable Annotations module
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    A function that returns the sum of two number with the
+    argument be type checked
+    """
+
+    return a + b
