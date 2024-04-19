f6180be3882d24553b8bc918c694f402f75e2571
Solomon <solomonuche@gmail.com>
2024-04-19 04:12:50 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Python - Variable Annotations module
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    A function that returns a concatenated string with the
+    argument be type checked
+    """
+
+    return f"{a}{b}"
