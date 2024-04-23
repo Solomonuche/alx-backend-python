96771c8ea47e225ec8a80b73bcfc402f2a96a75f
Solomon <solomonuche@gmail.com>
2024-04-23 05:23:12 +0100

    execute multiple coroutines at the same time with async

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -20,5 +20,6 @@ def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
+    total_time = end_time - start_time
 
-    return (end_time - start_time) / n
+    return total_time / n
