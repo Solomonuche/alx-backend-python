b2f96482c95791214a1166f4427a747f89d5b0c8
Solomon <solomonuche@gmail.com>
2024-05-18 02:12:46 +0100

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,7 +9,7 @@ from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -64,3 +64,37 @@ class TestGetJson(unittest.TestCase):
         # assert that the mocked request is calld once with a url
         mock_get.assert_called_once_with(url)
         self.assertEqual(result, payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    Parameterize and patch
+    """
+    def test_memoize(self):
+        """
+        test_memoize
+        """
+        class TestClass:
+            """
+            test class
+            """
+            def a_method(self):
+                """
+                return 42
+                """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ return a method
+                """
+                return self.a_method()
+
+        obj = TestClass()
+        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+            res_1 = obj.a_property
+            res_2 = obj.a_property
+
+            self.assertEqual(res_1, 42)
+            self.assertEqual(res_2, 42)
+            mock.assert_called_once()
